d8734abcb509fe4f0f3310327debc00ced978bd4
Lite_Mars <maxmassi12@gmail.com>
2019-10-14 19:35:20 +0200

    Enum changed

--- src/chess/File.java
+++ src/chess/File.java
@@ -10,5 +10,17 @@ public enum File {
     e,
     f,
     g,
-    h
+    h,
+    out;
+
+    public File add(int n) {
+    	int valore=(this.ordinal() + n);
+    	if(valore>7) return  File.out;
+    	return File.values()[valore];
+    }
+    public File sub(int n) {
+    	int valore=(this.ordinal() - n);
+    	if (valore<0)return File.out;
+    	return File.values()[valore];
+    }
 }
--- src/chess/Rank.java
+++ src/chess/Rank.java
@@ -1,13 +1,24 @@
 package chess;
 
-// horizontal row, labeled 1-8, starting from white's side of the board
-enum Rank {
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8
+public enum Rank {
+    first,
+    second,
+    third,
+    fourth,
+    fifth,
+    sixth,
+    seventh,
+    eighth,
+    out;
+	
+    public Rank add(int n) {
+    	int valore=(this.ordinal() + n);
+    	if(valore>7) return  Rank.out;
+    	return Rank.values()[valore];
+    }
+    public Rank sub(int n) {
+    	int valore=(this.ordinal() - n);
+    	if (valore<0)return Rank.out;
+    	return Rank.values()[valore];
+    }
 }
